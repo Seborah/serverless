2c2e99c67d8f7211cc7efb37677e1a6ffcf1bc9e
seborah <sebhase@gmail.com>
2022-06-25 02:17:35 -0400

    older veri

--- index.js
+++ index.js
@@ -22,12 +22,15 @@ functions.http("Sakura", async (req, res) => {
 	const signature = req.get("X-Signature-Ed25519")
 	const timestamp = req.get("X-Signature-Timestamp")
 	const body = req.rawBody // rawBody is expected to be a string, not raw bytes
+	var isVerified = false
 	try {
-		var isVerified = nacl.sign.detached.verify(Buffer.from(timestamp + body), Buffer.from(signature, "hex"), Buffer.from(PUBLIC_KEY, "hex"))
-	} catch (e) {
-		var isVerified = false
-	}
+		isVerified = nacl.sign.detached.verify(Buffer.from(timestamp + body), Buffer.from(signature, "hex"), Buffer.from(PUBLIC_KEY, "hex"))
+    } catch (e) { }
+    if (!isVerified) {
+        return res.status(401).send("invalid request signature")
+    }
 	if (req.rawBody) {
+		
 		var current = new Random({ data: req.rawBody.toString() })
 		await current.save()
 		var info = req.rawBody.toString()
@@ -37,9 +40,6 @@ functions.http("Sakura", async (req, res) => {
 			res.status(200)
 			res.send({ type: 1 })
 		} else if (data.type == 2) {
-			if (!isVerified) {
-				return res.status(401).send("invalid request signature")
-			}
 			//respond to slash commands
 			return res.send(commands.commands(data))
 		} else {
