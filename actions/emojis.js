14626795c9edd30195e24f12435c30d47ec0fe00
seborah <sebhase@gmail.com>
2022-11-17 00:16:50 -0500

    lf not crlf

--- actions/emojis.js
+++ actions/emojis.js
@@ -3,16 +3,15 @@ var axios = require("axios").default
 var endpoint = "https://discord.com/api/v10/guilds/{0}/emojis"
 
 async function getEmojis(token, serverID) {
-    try {
-        var response = await axios
-            .get(endpoint.replace("{0}", serverID.toString()), {
-                headers: {
-                    authorization: "Bot " + token,
-                },
-            })
-    } catch (err) {
-        console.log(err)
-        return null
-    }
-    return response.data
+	try {
+		var response = await axios.get(endpoint.replace("{0}", serverID.toString()), {
+			headers: {
+				authorization: "Bot " + token,
+			},
+		})
+	} catch (err) {
+		console.log(err)
+		return null
+	}
+	return response.data
 }
--- index.js
+++ index.js
@@ -5,36 +5,31 @@ const nacl = require("tweetnacl")
 
 var auth = JSON.parse(process.env.SAKURA)
 
-
-
 //! slow, adds extra import and is very large (cries)
 const mongoose = require("mongoose")
 mongoose.connect(auth.mongoUrl, { useNewUrlParser: true })
-
 
 // Your public key can be found on your application in the Developer Portal
 const PUBLIC_KEY = auth.publicToken
 
 commands.onStart()
 
-
 functions.http("Sakura", async (req, res) => {
-    
 	const signature = req.get("X-Signature-Ed25519")
 	const timestamp = req.get("X-Signature-Timestamp")
 	const body = req.rawBody // rawBody is expected to be a string, not raw bytes
 	var isVerified = false
 	try {
 		isVerified = nacl.sign.detached.verify(Buffer.from(timestamp + body), Buffer.from(signature, "hex"), Buffer.from(PUBLIC_KEY, "hex"))
-    } catch (e) { }
-    if (!isVerified) {
-        return res.status(401).send("invalid request signature")
-    }
+	} catch (e) {}
+	if (!isVerified) {
+		return res.status(401).send("invalid request signature")
+	}
 	if (req.rawBody) {
 		//! slow
 		var current = new Random({ data: req.rawBody.toString() })
-        await current.save()
-        
+		await current.save()
+
 		var info = req.rawBody.toString()
 		var data = JSON.parse(info)
 		if (data.type == 1) {
